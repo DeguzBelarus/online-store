2ad262c01d9b7dd4f150bebeb89cc55afe3bea7b
Alick Shabanovich <alikshabanovich@gmail.com>
2022-12-21 21:07:29 +0300

    :feat: add page404 component and markup

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,5 +1,26 @@
 import React, { FC } from 'react';
+import './Page404.scss';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 export const Page404: FC = (): JSX.Element => {
-  return <div className="page-404-wrapper">Page404</div>;
+  const navigate: NavigateFunction = useNavigate();
+  return (
+    <div className="page-404-wrapper">
+      <h1 className="page-404-h1">404 Error page</h1>
+      <section className="error-container">
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+        <span className="zero">
+          <span className="screen-reader-text">0</span>
+        </span>
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+      </section>
+      <button onClick={() => navigate('/')} className="more-link">
+        Home Page
+      </button>
+    </div>
+  );
 };
