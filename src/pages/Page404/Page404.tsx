a49b1760363ff1e0a21eb23ee184d8497a2b7629
Alick Shabanovich <alikshabanovich@gmail.com>
2022-12-23 02:36:16 +0300

    fix: redesign page404 markup and migrate page404 styles to scss

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -6,18 +6,8 @@ export const Page404: FC = (): JSX.Element => {
   const navigate: NavigateFunction = useNavigate();
   return (
     <div className="page-404-wrapper">
-      <h1 className="page-404-h1">404 Error page</h1>
-      <section className="error-container">
-        <span className="four">
-          <span className="screen-reader-text">4</span>
-        </span>
-        <span className="zero">
-          <span className="screen-reader-text">0</span>
-        </span>
-        <span className="four">
-          <span className="screen-reader-text">4</span>
-        </span>
-      </section>
+      <h1 className="page-404-h1">404</h1>
+      <h2 className="page-404-h2">Page not found</h2>
       <button onClick={() => navigate('/')} className="more-link">
         Home Page
       </button>
